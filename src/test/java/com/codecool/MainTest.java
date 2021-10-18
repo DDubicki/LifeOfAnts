9c13985f934de9164a9366f7e650da6332442411
DDubicki <dubickidymitr@gmail.com>
2021-10-18 18:07:04 +0200

    Initial project

--- src/test/java/com/codecool/MainTest.java
+++ src/test/java/com/codecool/MainTest.java
@@ -4,14 +4,8 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class MainTest {
+
     @Test
     public void shouldAnswerWithTrue()
     {
