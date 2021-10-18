2f9c44fda66da5689a46e81942e4eccd36e083fc
DDubicki <dubickidymitr@gmail.com>
2021-10-18 20:06:47 +0200

    Simulation done

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -3,6 +3,7 @@ package com.codecool;
 public class Main {
 
     public static void main( String[] args ) {
-
+        Simulation simulation = new Simulation();
+        simulation.start();
     }
 }
--- src/main/java/com/codecool/Simulation.java
+++ src/main/java/com/codecool/Simulation.java
@@ -1,6 +1,21 @@
 package com.codecool;
 
+import java.util.Scanner;
+
 public class Simulation {
+
+    private final Colony colony;
+
+    public Simulation() {
+        colony = new Colony(50);
+        colony.generateAnts(5, 10, 15);
+    }
+
     public void start() {
+        Scanner scanner = new Scanner(System.in);
+        do {
+            colony.update();
+            colony.display();
+        } while (!scanner.nextLine().equalsIgnoreCase("q"));
     }
 }
