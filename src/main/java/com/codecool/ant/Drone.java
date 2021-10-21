a0203218520278c3011719a0d7e7dfa355c91056
DDubicki <dubickidymitr@gmail.com>
2021-10-21 20:29:19 +0200

    Ants mating done

--- src/main/java/com/codecool/ant/Drone.java
+++ src/main/java/com/codecool/ant/Drone.java
@@ -9,18 +9,67 @@ public class Drone extends Ant {
 
     public static final char DRONE_SYMBOL = 'D';
     private Direction currentDirection;
+    private boolean isMating;
+    private int moodTurns = 0;
 
     public Drone(Position position) {
         super(position, DRONE_SYMBOL);
         this.currentDirection = Direction.getRandomDirection();
+        this.isMating = false;
     }
 
     @Override
     public void act(int width) {
         int queenPosition = width / 2;
+        int absDifferenceToQueenPositionX = Math.abs(queenPosition - getPosition().x);
+        int absDifferenceToQueenPositionY = Math.abs(queenPosition - getPosition().y);
+
+        if ((absDifferenceToQueenPositionX == 0 && absDifferenceToQueenPositionY == 1) || (absDifferenceToQueenPositionX == 1 && absDifferenceToQueenPositionY == 0)) {
+            standNearToQueen(width);
+        } else {
+            makeMove(queenPosition, width);
+        }
+    }
+
+    private void standNearToQueen(int width) {
+        if (Queen.isMatingMood()) {
+            Queen.setMating();
+            this.isMating = true;
+            moodTurns++;
+        } else if (this.isMating) {
+            if (moodTurns < 10) {
+                moodTurns++;
+            } else {
+                this.isMating = false;
+                moodTurns = 0;
+                kickOfAnt(width);
+            }
+        } else {
+            kickOfAnt(width);
+        }
+    }
+
+    private void kickOfAnt(int width) {
+        int positionX = 0;
+        int positionY = 0;
+        currentDirection = Direction.getRandomDirection();
+        if (currentDirection.differenceX == 0) {
+            positionY = currentDirection.differenceY == 1 ? width / 2 : -(width / 2);
+        }
+        else {
+            positionX = currentDirection.differenceX == 1 ? width / 2 : -(width / 2);
+        }
+        Position newPosition = new Position(positionX, positionY);
+        setPosition(newPosition);
+    }
+
+    private void makeMove(int queenPosition, int width) {
         int difX = getDifference(queenPosition, getPosition().x);
         int difY = getDifference(queenPosition, getPosition().y);
-        chooseCurrentMove(difX, difY, width);
+        do {
+            currentDirection = currentDirection.getCloserToQueenDirection(difX, difY);
+            position = position.nextPositionInDirection(Objects.requireNonNull(currentDirection));
+        } while (position.isOutBoardPosition(width));
     }
 
     private int getDifference(int queenPosition, int dronePosition) {
@@ -31,36 +80,5 @@ public class Drone extends Ant {
             differenceInPosition--;
         }
         return differenceInPosition;
-    }
-
-    private void chooseCurrentMove(int difX, int difY, int width) {
-
-        if (difX == 0 && difY == 0) {
-            kickOfAnt(width);
-        } else {
-            do {
-                makeMoveCloserToQueen(difX, difY);
-            } while (position.isOutBoardPosition(width));
-        }
-    }
-
-    private void kickOfAnt(int width) {
-        int positionX = 0;
-        int positionY = 0;
-        currentDirection = Direction.getRandomDirection();
-        if (currentDirection.differenceX == 0)
-            positionY = currentDirection.differenceY == 1 ? width / 2 : -(width / 2);
-        else
-            positionX = currentDirection.differenceX == 1 ? width / 2 : -(width / 2);
-        Position newPosition = new Position(positionX, positionY);
-        setPosition(newPosition);
-        if (Queen.isMatingMood()){
-            Queen.setMating();
-        }
-    }
-
-    private void makeMoveCloserToQueen(int difX, int difY) {
-        currentDirection = currentDirection.getCloserToQueenDirection(difX, difY);
-        position = position.nextPositionInDirection(Objects.requireNonNull(currentDirection));
     }
 }
